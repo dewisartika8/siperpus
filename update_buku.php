726650611fb4a4aa165b6bea71b3a8eef0307cac
Talenta Sri Manurung <ce318011@students.del.ac.id>
2019-11-21 11:40:55 +0700

    Update update_buku.php

--- update_buku.php
+++ update_buku.php
@@ -20,26 +20,31 @@
 			<td>:</td>
 			<td><input type="text" name="kd_buku" value="<?php echo $data['kd_buku'] ?>" readonly></td>
 		</tr>
+		
 		<tr>
 			<td>Judul Buku</td>
 			<td>:</td>
 			<td><input type="text" name="judul_buku" value="<?php echo $data['judul_buku']?>"></td>
 		</tr>
+		
 		<tr>
 			<td>Pengarang</td>
 				<td>:</td>
 				<td><input type="text" name="pengarang" value="<?php echo $data['pengarang']?>"></td>
 		</tr>
+		
 		<tr>
 			<td>Jenis Buku</td>
 				<td>:</td>
 				<td><input type="text" name="jenis_buku" value="<?php echo $data['jenis_buku']?>"></td>
 		</tr>
+		
 		<tr>
 			<td>Penerbit</td>
 				<td>:</td>
 				<td><input type="text" name="penerbit" value="<?php echo $data['penerbit']?>"></td>
 		</tr>
+		
 		<tr>
 			<td colspan="3"><input type="submit" name="action" value="update" href="kd_buku=<?php echo $data['kd_buku'];?>"></td>
 		</tr>
